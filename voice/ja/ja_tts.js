75967dfd3948327d36cf4e8e282faf5d79338d05
結衣 <16128011+hfmkwi@users.noreply.github.com>
2026-01-11 12:32:13 -0600

    Refactor number to audio file function

--- voice/ja/ja_tts.js
+++ voice/ja/ja_tts.js
@@ -599,45 +599,22 @@ function getAttentionString(type) {
 function ogg_dist(distance) {
 	if (distance == 0) {
 		return "";
-	} else if (distance < 20) {
-		return Math.floor(distance).toString() + ".ogg ";
-	} else if (distance < 1000 && (distance % 50) == 0) {
-		return distance.toString() + ".ogg ";
-	} else if (distance < 30) {
-		return "20.ogg " + ogg_dist(distance - 20);
-	} else if (distance < 40) {
-		return "30.ogg " + ogg_dist(distance - 30);
-	} else if (distance < 50) {
-		return "40.ogg " + ogg_dist(distance - 40);
-	} else if (distance < 60) {
-		return "50.ogg " + ogg_dist(distance - 50);
-	} else if (distance < 70) {
-		return "60.ogg " + ogg_dist(distance - 60);
-	} else if (distance < 80) {
-		return "70.ogg " + ogg_dist(distance - 70);
-	} else if (distance < 90) {
-		return "80.ogg " + ogg_dist(distance - 80);
-	} else if (distance < 100) {
-		return "90.ogg " + ogg_dist(distance - 90);
-	} else if (distance < 200) {
-		return "100.ogg " + ogg_dist(distance - 100);
-	} else if (distance < 300) {
-		return "200.ogg " + ogg_dist(distance - 200);
-	} else if (distance < 400) {
-		return "300.ogg " + ogg_dist(distance - 300);
-	} else if (distance < 500) {
-		return "400.ogg " + ogg_dist(distance - 400);
-	} else if (distance < 600) {
-		return "500.ogg " + ogg_dist(distance - 500);
-	} else if (distance < 700) {
-		return "600.ogg " + ogg_dist(distance - 600);
-	} else if (distance < 800) {
-		return "700.ogg " + ogg_dist(distance - 700);
-	} else if (distance < 900) {
-		return "800.ogg " + ogg_dist(distance - 800);
-	} else if (distance < 1000) {
-		return "900.ogg " + ogg_dist(distance - 900);
+	} else if (distance <= 20) {
+		return Math.floor(distance) + ".ogg ";
+	} else if (distance < 10000) {
+		let distance_interval;
+		if (distance < 100) {
+			// audio files exist for multiples of 5 on the interval (20, 100]…
+			// why aren’t these used?
+			distance_interval = 10;
+		} else if (distance < 1000) {
+			// there is no 950.ogg for some reason? who tests this stuff⁇
+			distance_interval = distance < 950 ? 50 : 100;
+		} else {
+			distance_interval = 1000;
+		}
+		return Math.floor(distance / distance_interval) * distance_interval + ".ogg " + ogg_dist(distance % distance_interval);
 	} else {
-		return ogg_dist(distance/1000) + "1000.ogg " + ogg_dist(distance % 1000);
+		return ogg_dist(distance / 1000) + "1000.ogg " + ogg_dist(distance % 1000);
 	}
 }
